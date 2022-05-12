20ffd717624c02f38958641731780a4f690f8367
mauroef <cypress90@gmail.com>
2022-05-11 22:30:48 -0300

    Seo to login component.

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -1,15 +1,23 @@
 import { Status } from '../../types/auth'
+import Seo from '../seo'
 
-const LoginScreen = ({ signIn, status }) => (
-  <div>
-    {/* TODO: include spinner on waiting */}
-    {status === Status.PENDING && (
-      <span>Intentando de restaurar sesión...</span>
-    )}
-    {status === Status.RESOLVED && (
-      <button onClick={signIn}>Iniciar sesión con Google</button>
-    )}
-  </div>
+const Login = ({ signIn, status }) => (
+  <>
+    <Seo
+      title='Iniciar Sesión'
+      description='Iniciar sesión para ingresar al sistema de registros'
+      image=''
+    />
+    <main>
+      {/* TODO: include spinner on waiting */}
+      {status === Status.PENDING && (
+        <span>Intentando de restaurar sesión...</span>
+      )}
+      {status === Status.RESOLVED && (
+        <button onClick={signIn}>Iniciar sesión con Google</button>
+      )}
+    </main>
+  </>
 )
 
-export default LoginScreen
+export default Login
